8d28add632f313bb1de57b9c4261925920f38cd9
Kennan Hunter <kennanhunter5@gmail.com>
2022-12-04 22:04:27 -0500

    day 1

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> elves = new List<int>() { 0 };
+        int elfIndex = 0;
+
+        using (StreamReader sr = File.OpenText("input.txt"))
+        {
+            string? s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                if (s == "")
+                {
+                    elfIndex += 1;
+                    elves.Add(0);
+                    continue;
+                }
+                elves[elfIndex] += Int32.Parse(s);
+            }
+        }
+
+        Console.WriteLine(elves.Max());
+    }
+}
